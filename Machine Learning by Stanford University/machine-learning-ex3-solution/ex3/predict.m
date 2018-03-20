80e2481d791163393d6709fb063b7e9466410405
Unknown <hxuanyu@gmail.com>
2018-03-20 11:49:46 +1100

    update

--- Machine Learning by Stanford University/machine-learning-ex3-solution/ex3/predict.m
+++ Machine Learning by Stanford University/machine-learning-ex3-solution/ex3/predict.m
@@ -7,12 +7,26 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
+
+X = [ones(m, 1) X];
+
+a2 = sigmoid(Theta1 * X');
+a2 = [ones(1, m); a2];
+
+output = (sigmoid(Theta2 * a2))';
+
+[max_, p] = max(output, [], 2);
+
+
+
+
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
