d9d39ddc417831ec878f53df75bc3888f1517761
Unknown <hxuanyu@gmail.com>
2018-04-16 15:48:38 +1000

    week 8

---
+++ Machine Learning by Stanford University/machine-learning-ex7-solution/ex7/findClosestCentroids.m
@@ -0,0 +1,39 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+
+for exampleid = 1:size(X, 1)
+   disvec = zeros(K, 1);
+   for cenid = 1:K
+      disvec(cenid) = sumsq(X(exampleid, :) - centroids(cenid, :));
+   end
+   [minval, row] = min(min(disvec,[],2));
+   idx(exampleid) = row;
+end
+
+
+
+
+% =============================================================
+
+end
